b0d7d26966c729c8903a0da3bd254cb4cb48a808
filippo-ferrando <ferrandodfilippo@gmail.com>
2020-02-07 20:19:43 +0100

    bug fix..again

--- daemon.sh
+++ daemon.sh
@@ -5,24 +5,40 @@
 while(1){
 
     python detection.py
+    
+    echo "detection..."
 
     delay 1m
 
-    sshpass -p <naopass> scp /home/pi/data/temperature.txt <user>@<ipnao>:<percorsoMemoriaNao>/temperature.txt
-    sshpass -p <naopass> scp /home/pi/data/humidity.txt <user>@<ipnao>:<percorsoMemoriaNao>/humidity.txt
-    sshpass -p <naopass> scp /home/pi/data/co2.txt <user>@<ipnao>:<percorsoMemoriaNao>/co2.txt
-
+    sshpass -p <naopass> scp /home/pi/temperature.txt <user>@<ipnao>:<percorsoMemoriaNao>/temperature.txt
+    sshpass -p <naopass> scp /home/pi/humidity.txt <user>@<ipnao>:<percorsoMemoriaNao>/humidity.txt
+    sshpass -p <naopass> scp /home/pi/co2.txt <user>@<ipnao>:<percorsoMemoriaNao>/co2.txt
+    
+    echo "Sending files in progress..."
+    
     delay 6m
 
-    rm /home/pi/data/temperature.txt
-    rm /home/pi/data/humidity.txt
-    rm /home/pi/data/co2.txt
+    cp -r /home/pi/temperature.txt /home/pi/rilevation/archive
+    cp -r /home/pi/humidity.txt /home/pi/rilevation/archive
+    cp -r /home/pi/co2.txt /home/pi/rilevation/archive
+
+    echo "Saving files..."
+
+    delay 6s
+
+    rm /home/pi/temperature.txt
+    rm /home/pi/humidity.txt
+    rm /home/pi/co2.txt
     
+    echo "Deleting previous local detection..."
+
     delay 2s
 
     sshpass -p <naopass> ssh <utente>@<naoip> "rm -rf <percorsoNao>/temperatura.txt"
     sshpass -p <naopass> ssh <utente>@<naoip> "rm -rf <percorsoNao>/humidity.txt"
     sshpass -p <naopass> ssh <utente>@<naoip> "rm -rf <percorsoNao>/co2.txt"
+
+    echo "Deleting previous remote detection..."
 
     delay 3s
 
