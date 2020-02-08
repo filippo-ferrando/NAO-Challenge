7e3e07ce6f5a002659647a0ee237d113a60aca2c
filippo-ferrando <ferrandodfilippo@gmail.com>
2020-02-08 21:33:24 +0100

    bug fix, comment

--- detection.py
+++ detection.py
@@ -1,18 +1,18 @@
-import Adafruit_DHT as dht
-import os
-import mh_z19
+import Adafruit_DHT as dht  #libreria dht22
+import os   #libreria controllo dei file
+import mh_z19   #libreria mhz19 (co2)
 
 h,t = dht.read_retry(dht.DHT22, 4) #4 pin del raspberry
-co2=mh_z19.read()
+co2=mh_z19.read()                  #schema per la connessione salvato nei preferiti
 
-temp=open("temperature.txt","w")
-temp.write(t)
+temp=open("temperature.txt","w")       #scrittura della variabile t nel file temperatura
+temp.write(repr(t))
 temp.close()
 
-hum=open("humidity.txt","w")
-hum.write(h)
+hum=open("humidity.txt","w")        #scrittura della variabile h nel file umidità
+hum.write(repr(h))
 temp.close()
 
-cco2=open("co2.txt","w")
-cco2.write(co2)
+cco2=open("co2.txt","w")        #scrittura della variabile co2 nel file co2
+cco2.write(repr(co2))
 cco2.close()
