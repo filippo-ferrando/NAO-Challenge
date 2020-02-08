2fe20b55fb419a20bc4c2fe87c3fc9c2656a8dc2
filippo-ferrando <ferrandodfilippo@gmail.com>
2020-02-08 21:34:18 +0100

    added installer for libraries

---
+++ installer.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+echo "Installing DHT 22 libraries..."
+
+sleep 1s
+
+echo "Cloning Repo..."
+
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+
+echo "Installing..."
+
+cd Adafruit_Python_DHT/
+
+sudo apt-get update
+sudo apt-get install build-essential python-dev python-openssl
+sudo python setup.py install
+
+sleep 1s
+
+clear
+
+echo "Installing mhz19 libraries..."
+
+sleep 1s
+
+pip install mh-z19
+
+sleep 1s
+
+clear
+
+echo "Installation complete ! " | figlet
