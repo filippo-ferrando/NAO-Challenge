16fabccfad033f1da05af53db4d0775197528540
filippo-ferrando <ferrandodfilippo@gmail.com>
2020-02-11 08:36:35 +0100

    added data saving feauture

--- daemon.sh
+++ daemon.sh
@@ -7,13 +7,18 @@ echo "Starting Daemon" | figlet #only a cool start
 #reverse shell for emergency connection, if it will not find any connection, it will pass over
 
 echo "Attempt reverse shell"
-rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1 | nc 192.168.1.244 4444 -q 10 > /tmp/f  #work-around for netcat -e option
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1 | nc <ipPcEmergenza> 4444 -q 10 > /tmp/f  #work-around for netcat -e option
 
 sleep 10s
 
 #cicle for sending saving and removing file (it also run the py script for retrive data)
 
 while true; do
+
+    #creazione variabile con data corrente(yyyy-mm-dd)
+    d=$(date -I)
+    #creazione variabile con ora (hh-mm)
+    o=$(date +%R)
 
     python detection.py #retrive data from sensor
     
@@ -26,20 +31,54 @@ while true; do
     sshpass -p <naopass> scp /home/pi/co2.txt <user>@<ipnao>:<percorsoMemoriaNao>/co2.txt
     
     echo "Sending files in progress..."
+
+    sleep 3s
+
+    #controllo se la cartella con la data esiste, se no la creo
+    if [ -d $d ]; then
+        echo "la cartella esiste già"
+    else
+        mkdir $d
+    fi
+
+    #rinomino i file delle misurazioni aggiungendo l'ora
+    mv /home/pi/temperature.txt /home/pi/temperature-$o.txt
+    mv /home/pi/humdity.txt /home/pi/humdity-$o.txt
+    mv /home/pi/co2.txt /home/pi/co2-$o.txt
+
+    #copio i file rinominati nella cartella con la data
+    cp -r /home/pi/temperature-$o.txt /home/pi/rilevation/$d    #saving file in archive directory
+    cp -r /home/pi/humidity-$o.txt /home/pi/rilevation/$d
+    cp -r /home/pi/co2-$o.txt /home/pi/rilevation/$d
+
+    echo "Saving files..."
     
     sleep 6m  #delay for the new detection
 
-    cp -r /home/pi/temperature.txt /home/pi/rilevation/archive      #saving file in archive directory
-    cp -r /home/pi/humidity.txt /home/pi/rilevation/archive
-    cp -r /home/pi/co2.txt /home/pi/rilevation/archive
+    #controllo se la cartella con la data esiste, se no la creo
+    if [ -d $d ]; then
+        echo "la cartella esiste già"
+    else
+        mkdir $d
+    fi
+
+    #rinomino i file delle misurazioni aggiungendo l'ora
+    mv /home/pi/temperature.txt /home/pi/temperature-$o.txt
+    mv /home/pi/humdity.txt /home/pi/humdity-$o.txt
+    mv /home/pi/co2.txt /home/pi/co2-$o.txt
+
+    #copio i file rinominati nella cartella con la data
+    cp -r /home/pi/temperature-$o.txt /home/pi/rilevation/$d    #saving file in archive directory
+    cp -r /home/pi/humidity-$o.txt /home/pi/rilevation/$d
+    cp -r /home/pi/co2-$o.txt /home/pi/rilevation/$d
 
     echo "Saving files..."
 
     sleep 6s
 
-    rm /home/pi/temperature.txt     #removing local files
-    rm /home/pi/humidity.txt
-    rm /home/pi/co2.txt
+    rm /home/pi/temperature-$o.txt     #removing local files
+    rm /home/pi/humidity-$o.txt
+    rm /home/pi/co2-$o.txt
     
     echo "Deleting previous local detection..."
 
@@ -54,3 +93,4 @@ while true; do
     sleep 3s
 
 done
+
